eea506cabb21f2d2200556e6f8bdc2dd89a41c25
Jordi Pont-Tuset <jponttuset@gmail.com>
2016-09-16 09:45:26 +0200

    - Empty case handled for mask2box

--- src/bboxes/mask2box.m
+++ src/bboxes/mask2box.m
@@ -14,10 +14,14 @@
 % Please consider citing the paper if you use this code.
 % ------------------------------------------------------------------------
 function bbox = mask2box( mask )
-    tmp = regionprops(double(mask),'BoundingBox'); % Double to force a single bbox
-    bbox(1) = tmp.BoundingBox(2)+0.5;
-    bbox(2) = tmp.BoundingBox(1)+0.5;
-    bbox(3) = tmp.BoundingBox(2)+tmp.BoundingBox(4)-0.5;
-    bbox(4) = tmp.BoundingBox(1)+tmp.BoundingBox(3)-0.5;
+    if ~any(mask(:))
+        bbox = [0 0 0 0];
+    else
+        tmp = regionprops(double(mask),'BoundingBox'); % Double to force a single bbox
+        bbox(1) = tmp.BoundingBox(2)+0.5;
+        bbox(2) = tmp.BoundingBox(1)+0.5;
+        bbox(3) = tmp.BoundingBox(2)+tmp.BoundingBox(4)-0.5;
+        bbox(4) = tmp.BoundingBox(1)+tmp.BoundingBox(3)-0.5;
+    end
 end
 
